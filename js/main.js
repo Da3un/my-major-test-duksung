2a3978f92c37ac9c2e882c8abebc78d7096e40fa
HwayoungYoon <54611046+HwayoungYoon@users.noreply.github.com>
2020-05-27 19:26:27 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -157,10 +157,10 @@ const goResult = () => {
     pTitle.innerHTML = u_name.value + ' 님에게 맞는 전공은';
     //res_point.innerHTML = point + '점';
     pin.style.marginLeft = infoList[grade].mLeft;
-    res_img.src = img_url;
+/*     res_img.src = img_url;
     res_img.alt = infoList[grade].name;
     res_img.title = infoList[grade].name;
-    res_img_div.appendChild(res_img);
+    res_img_div.appendChild(res_img); */
     animal.innerHTML = infoList[grade].name;
     desc.innerHTML = infoList[grade].desc;
 
